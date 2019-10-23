a6acabeaf7bd2ca0e3f038f912d176bd82bf41e5
root <root@localhost.localdomain>
2019-10-23 12:29:02 -0400

    Version one

---
+++ views/site/mpe.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'MP3s';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-mp3">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+	You can list to the Musics here.
+    </p>
+  <code><?= __FILE__ ?></code>
+</div>
